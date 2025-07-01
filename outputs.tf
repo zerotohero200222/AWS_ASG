9972bd818e30145c76ebe486f098376855d3d6e2
zerotohero200222 <aebharath17@gmail.com>
2025-07-01 11:45:13 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,11 @@ output "asg_name" {
 output "placement_group" {
   value = aws_placement_group.test.name
 }
+
+output "subnet_id" {
+  value = aws_subnet.example.id
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
