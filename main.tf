60a46473019888e295354d7e2c7e456a521992a4
zerotohero200222 <aebharath17@gmail.com>
2025-07-01 11:44:17 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,74 @@
-# Random tag value to use in resources
 resource "random_string" "tag_value" {
   length  = 6
-  special = false
   upper   = false
+  special = false
 }
 
-# Example VPC
+data "aws_availability_zones" "available" {}
+
 resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "main-vpc-${random_string.tag_value.result}"
-  }
 }
 
-# Example Subnet
 resource "aws_subnet" "example" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
-  availability_zone       = "us-east-2a"
+  availability_zone       = data.aws_availability_zones.available.names[0]
   map_public_ip_on_launch = true
+}
 
-  tags = {
-    Name = "subnet-${random_string.tag_value.result}"
+resource "aws_security_group" "lb_sg" {
+  name        = "lb-sg"
+  description = "Allow HTTP"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
+resource "aws_launch_configuration" "foobar" {
+  name_prefix   = "foobar-lc-"
+  image_id      = var.ami_id
+  instance_type = var.instance_type
+
+  security_groups = [aws_security_group.lb_sg.id]
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_placement_group" "test" {
+  name     = "test"
+  strategy = "cluster"
+}
+
+resource "aws_autoscaling_group" "bar" {
+  name                      = "foobar3-terraform-test"
+  max_size                  = 2
+  min_size                  = 1
+  desired_capacity          = 1
+  health_check_type         = "EC2"
+  health_check_grace_period = 300
+  force_delete              = true
+  placement_group           = aws_placement_group.test.id
+  launch_configuration      = aws_launch_configuration.foobar.name
+  vpc_zone_identifier       = [aws_subnet.example.id]
+
+  tag {
+    key                 = "Environment"
+    value               = "dev-${random_string.tag_value.result}"
+    propagate_at_launch = true
+  }
+}
