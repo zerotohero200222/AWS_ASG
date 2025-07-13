b187d3065b0def4e7799e9b9d1e80974ead725e7
zerotohero200222 <aebharath17@gmail.com>
2025-07-13 20:48:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,29 @@
-variable "instance_type" {
-  default = "t3.micro"
+variable "aws_region" {
+  description = "AWS region to deploy the resources"
+  type        = string
 }
 
 variable "ami_id" {
   description = "Amazon Machine Image ID"
-  default     = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI in your region
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "max_size" {
+  description = "Maximum number of instances in ASG"
+  type        = number
+}
+
+variable "min_size" {
+  description = "Minimum number of instances in ASG"
+  type        = number
+}
+
+variable "desired_capacity" {
+  description = "Desired number of instances in ASG"
+  type        = number
 }
