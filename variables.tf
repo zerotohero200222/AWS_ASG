a9a4a5effeffa96c5a3c8e7cb551e0449cc44ef4
zerotohero200222 <aebharath17@gmail.com>
2025-07-13 20:15:38 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
 variable "instance_type" {
-  default = "c5.large"
+  default = "t3.micro"
 }
 
 variable "ami_id" {
   description = "Amazon Machine Image ID"
-  default     = "ami-0c55b159cbfafe1f0" # Use a valid AMI ID for your region
+  default     = "ami-0c55b159cbfafe1f0" # Replace with a valid AMI in your region
 }
